6aa1c07ef9cc18875b58c003f9e4a074bf196672
Vinnie Falco <vinnie.falco@gmail.com>
2025-12-21 16:45:39 -0800

    chore: files

--- src/detail/except.cpp
+++ src/detail/except.cpp
@@ -9,6 +9,7 @@
 
 #include <boost/capy/detail/config.hpp>
 #include <boost/capy/detail/except.hpp>
+#include <boost/system/system_error.hpp>
 #include <boost/throw_exception.hpp>
 #include <stdexcept>
 #include <typeinfo>
@@ -25,19 +26,84 @@ throw_bad_typeid(
 }
 
 void
-throw_invalid_argument(
-    core::string_view s,
+throw_bad_alloc(
     source_location const& loc)
 {
-    throw_exception(std::invalid_argument(s), loc);
+    throw_exception(
+        std::bad_alloc(), loc);
+}
+
+void
+throw_invalid_argument(
+    source_location const& loc)
+{
+    throw_exception(
+        std::invalid_argument(
+            "invalid argument"),
+        loc);
+}
+
+void
+throw_invalid_argument(
+    char const* what,
+    source_location const& loc)
+{
+    throw_exception(
+        std::invalid_argument(what), loc);
+}
+
+void
+throw_length_error(
+    source_location const& loc)
+{
+    throw_exception(
+        std::length_error(
+            "length error"), loc);
+}
+
+void
+throw_length_error(
+    char const* what,
+    source_location const& loc)
+{
+    throw_exception(
+        std::length_error(what), loc);
 }
 
 void
 throw_logic_error(
-    core::string_view s,
     source_location const& loc)
 {
-    throw_exception(std::logic_error(s), loc);
+    throw_exception(
+        std::logic_error(
+            "logic error"),
+        loc);
+}
+
+void
+throw_out_of_range(
+    source_location const& loc)
+{
+    throw_exception(
+        std::out_of_range("out of range"), loc);
+}
+
+void
+throw_runtime_error(
+    char const* what,
+    source_location const& loc)
+{
+    throw_exception(
+        std::runtime_error(what), loc);
+}
+
+void
+throw_system_error(
+    system::error_code const& ec,
+    source_location const& loc)
+{
+    throw_exception(
+        system::system_error(ec), loc);
 }
 
 } // detail
