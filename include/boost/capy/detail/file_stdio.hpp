3e93be27e3a316583d60d1f8a192016161f3f8a8
Vinnie Falco <vinnie.falco@gmail.com>
2025-12-21 16:45:39 -0800

    chore: files

---
+++ include/boost/capy/detail/file_stdio.hpp
@@ -0,0 +1,92 @@
+//
+// Copyright (c) 2022 Vinnie Falco (vinnie.falco@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/cppalliance/capy
+//
+
+#ifndef BOOST_CAPY_DETAIL_FILE_STDIO_HPP
+#define BOOST_CAPY_DETAIL_FILE_STDIO_HPP
+
+#include <boost/capy/detail/config.hpp>
+#include <boost/capy/error.hpp>
+#include <boost/capy/file_mode.hpp>
+#include <cstdio>
+#include <cstdint>
+
+namespace boost {
+namespace capy {
+namespace detail {
+
+// Implementation of File which uses cstdio.
+class file_stdio
+{
+    std::FILE* f_ = nullptr;
+
+public:
+    using native_handle_type = std::FILE*;
+
+    BOOST_CAPY_DECL
+    ~file_stdio();
+
+    file_stdio() = default;
+
+    BOOST_CAPY_DECL
+    file_stdio(file_stdio&& other) noexcept;
+
+    BOOST_CAPY_DECL
+    file_stdio&
+    operator=(file_stdio&& other) noexcept;
+
+    std::FILE*
+    native_handle() const
+    {
+        return f_;
+    }
+
+    BOOST_CAPY_DECL
+    void
+    native_handle(std::FILE* f);
+
+    bool
+    is_open() const
+    {
+        return f_ != nullptr;
+    }
+
+    BOOST_CAPY_DECL
+    void
+    close(system::error_code& ec);
+
+    BOOST_CAPY_DECL
+    void
+    open(char const* path, file_mode mode, system::error_code& ec);
+
+    BOOST_CAPY_DECL
+    std::uint64_t
+    size(system::error_code& ec) const;
+
+    BOOST_CAPY_DECL
+    std::uint64_t
+    pos(system::error_code& ec) const;
+
+    BOOST_CAPY_DECL
+    void
+    seek(std::uint64_t offset, system::error_code& ec);
+
+    BOOST_CAPY_DECL
+    std::size_t
+    read(void* buffer, std::size_t n, system::error_code& ec);
+
+    BOOST_CAPY_DECL
+    std::size_t
+    write(void const* buffer, std::size_t n, system::error_code& ec);
+};
+
+} // detail
+} // capy
+} // boost
+
+#endif
---
+++ include/boost/capy/error.hpp
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2025 Vinnie Falco (vinnie.falco@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/cppalliance/capy
+//
+
+#ifndef BOOST_CAPY_ERROR_HPP
+#define BOOST_CAPY_ERROR_HPP
+
+#include <boost/capy/detail/config.hpp>
+#include <boost/system/error_code.hpp>
+
+namespace boost {
+namespace capy {
+
+} // capy
+} // boost
+
+#endif
